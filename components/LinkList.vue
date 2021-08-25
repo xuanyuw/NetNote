2ea767395228f8dd13c324f65933969251ebb532
Xuanyu Wu <45672961+xuanyuw@users.noreply.github.com>
2021-08-25 17:33:06 +0800

    list component WIP

---
+++ components/LinkList.vue
@@ -0,0 +1,11 @@
+<template>
+  <CList styleType="disc">
+    <CListItem v-for="item in ListItems" v-bind:key="item.id" />
+  </CList>
+</template>
+
+<script lang="js">
+export default {
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,81 +8,14 @@
       flex-dir="column"
       justify-content="center"
     >
-      <CHeading text-align="center" mb="4">
-        ⚡️ Hello chakra-ui/vue
-      </CHeading>
+      <CHeading text-align="center" mb="4"> NetNote </CHeading>
       <CFlex justify="center" direction="column" align="center">
-        <CBox mb="3">
-          <CIconButton
-            mr="3"
-            :icon="colorMode === 'light' ? 'moon' : 'sun'"
-            :aria-label="`Switch to ${
-              colorMode === 'light' ? 'dark' : 'light'
-            } mode`"
-            @click="toggleColorMode"
-          />
-          <CButton
-            left-icon="info"
-            variant-color="blue"
-            @click="showToast"
-          >
-            Show Toast
-          </CButton>
-        </CBox>
-        <CAvatarGroup>
-          <CAvatar
-            name="Evan You"
-            alt="Evan You"
-            src="https://pbs.twimg.com/profile_images/1206997998900850688/cTXTQiHm_400x400.jpg"
-          >
-            <CAvatarBadge size="1.0em" bg="green.500" />
-          </CAvatar>
-          <CAvatar
-            name="Jonathan Bakebwa"
-            alt="Jonathan Bakebwa"
-            src="https://res.cloudinary.com/xtellar/image/upload/v1572857445/me_zqos4e.jpg"
-          >
-            <CAvatarBadge size="1.0em" bg="green.500" />
-          </CAvatar>
-          <CAvatar
-            name="Segun Adebayo"
-            alt="Segun Adebayo"
-            src="https://pbs.twimg.com/profile_images/1169353373012897802/skPUWd6e_400x400.jpg"
-          >
-            <CAvatarBadge size="1.0em" bg="green.500" />
-          </CAvatar>
-          <CAvatar src="pop">
-            <CAvatarBadge size="1.0em" border-color="papayawhip" bg="tomato" />
-          </CAvatar>
-        </CAvatarGroup>
-        <CButton
-          left-icon="close"
-          variant-color="red"
-          mt="3"
-          @click="showModal = true"
-        >
-          Delete Account
-        </CButton>
-        <CModal :is-open="showModal">
-          <CModalOverlay />
-          <CModalContent>
-            <CModalHeader>Are you sure?</CModalHeader>
-            <CModalBody>Deleting user cannot be undone</CModalBody>
-            <CModalFooter>
-              <CButton @click="showModal = false">
-                Cancel
-              </CButton>
-              <CButton
-                margin-left="3"
-                variant-color="red"
-                @click="showModal = false"
-              >
-                Delete User
-              </CButton>
-            </CModalFooter>
-            <CModalCloseButton @click="showModal = false" />
-          </CModalContent>
-        </CModal>
+        <CInputGroup w="50vw">
+          <CInput placeholder="Type a topic name here" size="lg" />
+          <CInputRightElement>
+            <CIconButton aria-label="Search database" icon="arrow-forward" />
+          </CInputRightElement>
+        </CInputGroup>
       </CFlex>
     </CBox>
   </div>
@@ -91,40 +24,20 @@
 <script lang="js">
 import {
   CBox,
-  CButton,
-  CAvatarGroup,
-  CAvatar,
-  CAvatarBadge,
-  CModal,
-  CModalContent,
-  CModalOverlay,
-  CModalHeader,
-  CModalFooter,
-  CModalBody,
-  CModalCloseButton,
   CIconButton,
   CFlex,
-  CHeading
+  CHeading,
+  CInput, CInputGroup, CInputRightElement,
 } from '@chakra-ui/vue'
 
 export default {
   name: 'App',
   components: {
     CBox,
-    CButton,
-    CAvatarGroup,
-    CAvatar,
-    CAvatarBadge,
-    CModal,
-    CModalContent,
-    CModalOverlay,
-    CModalHeader,
-    CModalFooter,
-    CModalBody,
-    CModalCloseButton,
     CIconButton,
     CFlex,
-    CHeading
+    CHeading,
+    CInput, CInputGroup, CInputRightElement,
   },
   inject: ['$chakraColorMode', '$toggleColorMode'],
   data () {
